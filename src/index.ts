8a9f9c6aedf0e28821fe039fd940a166d33d6170
Ruo feng <41161634+RuoFeng2015@users.noreply.github.com>
2026-01-20 16:34:53 +0800

    build: 图标更新 v1.3.6
    2222
    
    自动合并来自 Figma 插件的图标更新

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,12 @@
 
 import { IconBaoGaoZhuShou } from './icons/IconBaoGaoZhuShou';
 import { IconFrame1261156242 } from './icons/IconFrame1261156242';
+import { IconSouSuoZhuShou } from './icons/IconSouSuoZhuShou';
 import { IconXinXiGuanLi } from './icons/IconXinXiGuanLi';
 
 export { IconBaoGaoZhuShou, type IconBaoGaoZhuShouProps } from './icons/IconBaoGaoZhuShou';
 export { IconFrame1261156242, type IconFrame1261156242Props } from './icons/IconFrame1261156242';
+export { IconSouSuoZhuShou, type IconSouSuoZhuShouProps } from './icons/IconSouSuoZhuShou';
 export { IconXinXiGuanLi, type IconXinXiGuanLiProps } from './icons/IconXinXiGuanLi';
 
 /**
@@ -19,6 +21,7 @@ export { IconXinXiGuanLi, type IconXinXiGuanLiProps } from './icons/IconXinXiGua
 export const allIcons = {
   IconBaoGaoZhuShou,
   IconFrame1261156242,
+  IconSouSuoZhuShou,
   IconXinXiGuanLi
 } as const;
 
