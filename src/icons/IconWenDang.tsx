5006c48239db37fa029ee6e4cbb0309fd765dca1
Ruo feng <41161634+RuoFeng2015@users.noreply.github.com>
2026-01-21 23:39:43 +0800

    build: 图标更新 v1.5.16
    更新/新增 3 个图标：展开, 文件, 文档 图标
    
    自动合并来自 Figma 插件的图标更新

--- src/icons/IconWenDang.tsx
+++ src/icons/IconWenDang.tsx
@@ -23,11 +23,12 @@ export const IconWenDang = forwardRef<SVGSVGElement, IconWenDangProps>(
         width={size}
         height={size}
         viewBox="0 0 24 24"
+        fill="none"
         className={className}
-        style={style}
+        style={{ color, ...style }}
         {...props}
       >
-        <path fill="#F5F5F5" d="M0 0h24v24H0z"/><path fill="white" d="M-554.333-136.375h793v822h-793z"/><path fill="#4A4A4A" stroke="#4A4A4A" d="M16 3h-5v18h5zm5 0h-5v18h5zM5 3l4 .5L7.25 21 3 20.5z"/><path stroke="white" d="M18.5 9V7.5m-5 1.5V7.5"/>
+        <path fill="#4A4A4A" stroke="#4A4A4A" strokeLinejoin="round" strokeWidth="1.5" d="M16 3h-5v18h5z"/><path fill="#4A4A4A" stroke="#4A4A4A" strokeLinejoin="round" strokeWidth="1.5" d="M21 3h-5v18h5z"/><path fill="#4A4A4A" stroke="#4A4A4A" strokeLinejoin="round" strokeWidth="1.5" d="m5 3 4 .5L7.25 21 3 20.5z"/><path stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M18.5 9V7.5"/><path stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M13.5 9V7.5"/>
       </svg>
     );
   }
