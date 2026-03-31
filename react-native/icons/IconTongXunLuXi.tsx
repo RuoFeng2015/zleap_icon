5890581ab3deb885ffbe88c132c83eec03d14eed
Ruo feng <41161634+RuoFeng2015@users.noreply.github.com>
2026-03-31 18:34:11 +0800

    build: 图标更新 v0.0.72
    同步 2 个图标：清除/已读, 通讯录-细 图标
    
    自动合并来自 Figma 插件的图标更新

--- react-native/icons/IconTongXunLuXi.tsx
+++ react-native/icons/IconTongXunLuXi.tsx
@@ -2,15 +2,15 @@ import React, { forwardRef, useMemo } from 'react'
 import type { ComponentProps } from 'react'
 import { SvgXml } from 'react-native-svg'
 
-export interface IconSheZhi1Props extends Omit<ComponentProps<typeof SvgXml>, 'xml' | 'width' | 'height'> {
+export interface IconTongXunLuXiProps extends Omit<ComponentProps<typeof SvgXml>, 'xml' | 'width' | 'height'> {
   size?: number | string
   color?: string
 }
 
-export const IconSheZhi1 = forwardRef<unknown, IconSheZhi1Props>(
+export const IconTongXunLuXi = forwardRef<unknown, IconTongXunLuXiProps>(
   ({ size = 24, color, ...props }, ref) => {
     const baseXml = useMemo(
-      () => `<svg fill="none" viewBox="0 0 24 24"><path stroke="#0D131A" stroke-linejoin="round" stroke-width="1.5" d="M9.142 21.586a10 10 0 0 1-4.348-2.652 3 3 0 0 0-2.59-4.919A10.04 10.04 0 0 1 2.457 9H2.5a3 3 0 0 0 2.692-4.325 10 10 0 0 1 4.134-2.313 3 3 0 0 0 5.348 0 10 10 0 0 1 4.134 2.313A3 3 0 0 0 21.542 9a10.04 10.04 0 0 1 .255 5.015 3 3 0 0 0-2.59 4.919 10 10 0 0 1-4.349 2.652 3.001 3.001 0 0 0-5.716 0Z"/><path stroke="#0D131A" stroke-linejoin="round" stroke-width="1.5" d="M12 15.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Z"/></svg>`,
+      () => `<svg fill="none" viewBox="0 0 24 24"><path stroke="#3F3F46" stroke-linejoin="round" stroke-width="1.2" d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10Z"/><path stroke="#3F3F46" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.2" d="M12 8v8"/><path stroke="#3F3F46" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.2" d="M8 12h8"/></svg>`,
       [],
     )
 
@@ -48,6 +48,6 @@ export const IconSheZhi1 = forwardRef<unknown, IconSheZhi1Props>(
   },
 )
 
-IconSheZhi1.displayName = 'IconSheZhi1'
+IconTongXunLuXi.displayName = 'IconTongXunLuXi'
 
-export default IconSheZhi1
+export default IconTongXunLuXi
